11baf8359dae7ea7511047eb78f092132126c8f7
Lukasz <luktul13@gmail.com>
2020-02-10 20:29:41 +0100

    Added corrections.

--- src/runner.py
+++ src/runner.py
@@ -1,14 +1,13 @@
 import torch
-from torch import tensor
 
 
 def run():
     print(torch.cuda.get_device_name(0))
-    x = tensor([[1.0], [2.0], [3.0]],
-               requires_grad=True)
-    A = tensor([[1.0, 1.0, 1.0],
-                [1.0, 1.0, 1.0],
-                [1.0, 1.0, 1.0]], requires_grad=True)
+    x = torch.tensor([[1.0], [2.0], [3.0]],
+                     requires_grad=True)
+    A = torch.tensor([[1.0, 1.0, 1.0],
+                      [1.0, 1.0, 1.0],
+                      [1.0, 1.0, 1.0]], requires_grad=True)
     print(x.shape)
     print(A.shape)
     out = torch.matmul(A, x)
